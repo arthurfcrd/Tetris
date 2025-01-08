a180e5cb86d403f23073bcd2a14472d32e0a322e
Arthur <arthur.foucard@ensta-paris.fr>
2025-01-08 11:13:04 +0100

    moved constructor definition to cpp file

--- src/tetromino.cpp
+++ src/tetromino.cpp
@@ -1,6 +1,63 @@
 #include "tetromino.hpp"
 #include "grid.hpp"
 
+Tetromino::Tetromino(TetrominoType tetroType){ // TODO : change pos to be not hardcoded
+        type = tetroType;
+        pos = {5, 1};
+        switch(tetroType){
+            case TetrominoType::I:
+                tiles[0] = {-2, -1};
+                tiles[1] = {-1, -1};
+                tiles[2] = {1, -1};
+                tiles[3] = {2, -1};
+                color = Color::CYAN;
+                break;
+            case TetrominoType::O:
+                tiles[0] = {-1, 1};
+                tiles[1] = {1, -1};
+                tiles[2] = {-1, -1};
+                tiles[3] = {1, 1};
+                color = Color::YELLOW;
+                break;
+            case TetrominoType::T:
+                tiles[0] = {-1, 0};
+                tiles[1] = {0, 0};
+                tiles[2] = {0, -1};
+                tiles[3] = {1, 0};
+                color = Color::MAGENTA;
+                break;
+            case TetrominoType::S:
+                tiles[0] = {-1, 0};
+                tiles[1] = {0, 0};
+                tiles[2] = {0, -1};
+                tiles[3] = {1, -1};
+                color = Color::GREEN;
+                break;
+            case TetrominoType::Z:
+                tiles[0] = {-1, -1};
+                tiles[1] = {0, -1};
+                tiles[2] = {0, 0};
+                tiles[3] = {1, 0};
+                color = Color::RED;
+                break;
+            case TetrominoType::J:
+                tiles[0] = {-1, -1};
+                tiles[1] = {-1, 0};
+                tiles[2] = {0, 0};
+                tiles[3] = {1, 0};
+                color = Color::BLUE;
+                break;
+            case TetrominoType::L:
+                tiles[0] = {-1, 0};
+                tiles[1] = {0, 0};
+                tiles[2] = {1, 0};
+                tiles[3] = {1, -1};
+                color = Color::ORANGE;
+                break;
+        }
+
+    }
+
 bool isValidTile(Point point, const Grid& g){ // checks if a tile is within the grid and is not occupied
     return point.x >= 0 && point.x < g.width && point.y >= 0 && point.y < g.height && g.grid[point.x][point.y] == Color::NONE;
 }
